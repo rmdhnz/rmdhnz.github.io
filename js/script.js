6b2381bcc9af98fb28effe97210ca73202032794
rmdhnz <ramadhanfadhly2@gmail.com>
2022-07-21 23:52:18 +0700

    mengedit saketek

--- js/script.js
+++ js/script.js
@@ -8,3 +8,20 @@ $('.page-scroll').on('click',function(e){
     },1000);
     e.preventDefault();
 });
+
+
+//parallax
+$(window).scroll(function(){
+    var wScroll = $(this).scrollTop();
+
+    $('.jumbotron img').css({
+        'transform' : 'translate(0px,'+ wScroll/4 +'%)'
+    });
+    $('.jumbotron h1').css({
+        'transform' : 'translate(0px,'+ wScroll/2 +'%)'
+    });
+    $('.jumbotron p').css({
+        'transform' : 'translate(0px,'+ wScroll/1.2 +'%)'
+    });
+});
+
